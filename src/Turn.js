83136fbe457cd5a07901534f90b9551d82c0b8cc
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-08-25 19:46:04 -0600

    Add completed Iteration 1

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,25 @@ class Turn {
         this.userGuess = userGuess;
         this.card = cardObj;
     }
+
+    returnGuess = () => {
+        return this.userGuess;
+    }
+
+    returnCard = () => {
+        console.log(this.card);
+        return this.card;
+    }
+
+    evaluateGuess = () => {
+        return this.userGuess === this.card.correctAnswer;
+    }
+
+    giveFeedback = () =>{
+        if (this.evaluateGuess()) {
+            return 'correct!';
+        } else return 'incorrect!';
+    }
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,21 +6,69 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-    it.skip('should be a function', function() {
-        const turn = new Turn();
+    let card, turn1, turn2;
+
+    beforeEach(function() {
+
+        card = new Card(1, 'Which would you use to return one single value?', ['filter', 'map', 'reduce'], 'reduce');
+        turn1 = new Turn('map', card);
+        turn2 = new Turn('reduce', card);
+    });
+
+    it('should be a function', function() {
+
         expect(Turn).to.be.a('function');
     });
 
-    it.skip('should be an instance of Turn', function() {
-        const turn = new Turn();
-        expect(turn).to.be.an.instanceOf(Turn);
+    it('should be an instance of Turn', function() {
+
+        expect(turn1).to.be.an.instanceOf(Turn);
     });
 
     it('should store a user\'s guess to the question', function() {
-        const card = new Card(1, 'Which would you use to return one single value?', ['filter', 'map', 'reduce'], 'reduce');
-        const turn = new Turn('map', card);
 
-        expect(turn).to.be.
+        expect(turn1.userGuess).to.equal('map');
+    });
+
+    it('should store an instance of Card', function() {
+
+        expect(turn1.card).to.be.an.instanceOf(Card);
+    })
+
+    it('Should return the user\'s guess', function() {
+
+        const guess1 = turn1.returnGuess();
+        const guess2 = turn2.returnGuess();
+
+        expect(guess1).to.equal('map');
+        expect(guess2).to.equal('reduce');
+    })
+
+    it('Should return the card', function() {
+
+        const cardHolder1 = turn1.returnCard();
+        const cardHolder2 = turn2.returnCard();
+
+        expect(cardHolder1).to.equal(card);
+        expect(cardHolder2).to.equal(card);
+    })
+
+    it('should evaluate guess', function() {
+        
+        const guess1 = turn1.evaluateGuess();
+        const guess2 = turn2.evaluateGuess();
+
+        expect(guess1).to.equal(false);
+        expect(guess2).to.equal(true);
+    });
+
+    it('should give feedback based on user\'s guess', function() {
+        
+        const guess1 = turn1.giveFeedback();
+        const guess2 = turn2.giveFeedback();
+
+        expect(guess1).to.equal('incorrect!');
+        expect(guess2).to.equal('correct!');
     });
 
 });
